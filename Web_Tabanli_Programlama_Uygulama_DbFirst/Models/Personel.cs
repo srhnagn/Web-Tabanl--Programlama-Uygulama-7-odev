a447a84137ea146a98956224a8b58edbd0a65e08
Serhan Agan <serhanagan68@gmail.com>
2026-04-23 17:05:53 +0300

    chore: generate build artifacts and dependency cache for project build

---
+++ Web_Tabanli_Programlama_Uygulama_DbFirst/Models/Personel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Web_Tabanli_Programlama_Uygulama_DbFirst.Models;
+
+public partial class Personel
+{
+    public int Id { get; set; }
+
+    public string Ad { get; set; } = null!;
+
+    public string Soyad { get; set; } = null!;
+
+    public string Departman { get; set; } = null!;
+
+    public string Eposta { get; set; } = null!;
+
+    public string Telefon { get; set; } = null!;
+
+    public string Sehir { get; set; } = null!;
+
+    public DateTime CreatedDate { get; set; }
+}
